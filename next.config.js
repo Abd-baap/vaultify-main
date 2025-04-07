39e5bcf9e96c8196423974c6da245289b4c4298f
Abd-baap <abhigyaworkspace@gmail.com>
2025-04-07 19:44:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/(.*)',
-        has: [
-          {
-            type: 'host',
-            value: 'bitaegiris.vercel.app',
-          },
-        ],
-        permanent: true,
-        destination: 'https://bitaegiris.vercel.app/:path*',
-      },
-    ];
-  },
+  
   async headers() {
     return [
       {
