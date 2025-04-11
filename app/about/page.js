a80ed02759c459dea961df0b69f61611bd72b89b
Abd-baap <abhigyaworkspace@gmail.com>
2025-04-11 09:17:02 -0700

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -35,6 +35,52 @@ const page = () => {
             </ul>
         </div>
         </div>
+                      <div className='w-[100%] h-[100%] flex flex-col text-center justify-center my-8 p-8 items-center'>
+        <h1 className="text-2xl font-bold text-blue-700 font-bold">What we do to your password</h1>
+        <ol className="flex flex-col list-decimal pl-5 w-[100%] my-8 p-4 items-center gap-6">
+          <div className="masterdoe flex flex-col justify-center items-center w-[100%] gap-4 ">
+            <span className='text-xl text-left flex justify-start'>We hashed your mastercode using this code:</span>
+            <pre className='bg-zinc-900 shadow-xl text-white p-4 rounded-md leading-relaxed overflow-x-hidden overflow-y-auto whitespace-pre-wrap gap-2 w-[80%]'>   <code>const hashedCode = await bcrypt.hash(code, 10);
+            </code></pre>
+          </div>
+          <div className="submit function flex justify-center items-center flex-col w-[100%] gap-4 ">
+            <span className='text-xl'>We customize your password before hashing using this code:</span>
+            <pre className='bg-zinc-900 shadow-xl text-white p-4 rounded-md leading-relaxed overflow-x-hidden overflow-y-auto whitespace-pre-wrap gap-2 w-[80%]'> <code>const raw = JSON.stringify(
+              ...newPassword,
+              password: form.mastercode + 'V@u|t!y' + form.password,
+              user: User?.user?.id
+              );
+
+            </code></pre>
+          </div>
+          <div className="hasing flex justify-center items-center flex-col w-[100%] gap-4 ">
+            <span className='text-xl'>We encrypt your password  using this code:</span>
+            <pre className='bg-zinc-900 shadow-xl text-white p-4 rounded-md leading-relaxed overflow-x-hidden overflow-y-auto whitespace-pre-wrap gap-2 w-[80%]'> <code> const secretSalt = process.env.SECRET_SALT; // Extra security layer
+<br/>// we cant disclose key and iv for security reason but they are formed using the mastercode
+<br/>
+              const cipher = crypto.createCipheriv(algorithm, key, iv);<br/>
+              let encrypted = cipher.update(body.password, "utf8", "hex");<br/>
+              encrypted += cipher.final("hex");<br/>
+
+
+            </code></pre>
+          </div>
+          <div className="decrypting flex justify-center items-center flex-col w-[100%] gap-4 ">
+            <span className='text-xl'>When you enter correct mastercode we decrypt it and displays it using this code:</span>
+            <pre className='bg-zinc-900 shadow-xl text-white p-4 rounded-md leading-relaxed overflow-x-hidden overflow-y-auto whitespace-pre-wrap gap-2 w-[80%]'> <code>
+              //We are sorry we cant disclose key and iv for security reason but they are formed using the mastercode<br/>
+                    const decipher = crypto.createDecipheriv(algorithm, storedKey, providedIv);<br/>
+    
+                    let decrypted = decipher.update(element.password, "hex", "utf8");<br/>
+                    decrypted += decipher.final("utf8");<br/>
+    
+                    return  ...element, password: decrypted ;
+
+            </code></pre>
+          </div>
+        </ol>
+      </div>
+    
 <div className="projects gap-4 my-8  p-8 shadow-sm hover:shadow-xl w-[100%]">
     <span className="head text-2xl font-bold text-blue-700">Beyond BitAegiris: Our Digital Ecosystem</span>
     <div className="project flex text-xl">
