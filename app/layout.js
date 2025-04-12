02ab0b4b911a1bd0157744720d38dbf50166a376
Abd-baap <abhigyaworkspace@gmail.com>
2025-04-12 17:35:48 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -28,8 +28,7 @@ export default function RootLayout({ children }) {
         <Analytics />
         <head>
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6836466532865002"
-     crossorigin="anonymous"></script>
-          <meta name="google-adsense-account" content="ca-pub-6836466532865002" />
+     crossorigin="anonymous"></script> <meta name="google-adsense-account" content="ca-pub-6836466532865002" />
           <meta name="robots" content="index, follow" />
           <meta name="google-site-verification" content="MIDLOYFbs4SS5EOC6oQcQd2CU6R4U0gxy70ESi1oLew" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
