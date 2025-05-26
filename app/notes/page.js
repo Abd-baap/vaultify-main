a9e286dc8af512a95c97ddc03b5774e05e9c7afd
Abd-baap <abhigyaworkspace@gmail.com>
2025-05-26 22:42:27 +0530

    Update page.js

--- app/notes/page.js
+++ app/notes/page.js
@@ -1,18 +1,13 @@
 import React from 'react'
 import Notes from '../components/notes'
-import { ClerkProvider,SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs';
 
 
 const page = () => {
   return (
     <div>
-     <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
-      
-    <SignedIn>
+     
       <Notes/>
-    </SignedIn>
+    
     </div>
   )
 }
