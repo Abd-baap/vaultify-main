3226fb936064872a852d53bf26399e614e11555d
Abd-baap <abhigyaworkspace@gmail.com>
2025-05-26 22:45:09 +0530

    Update layout.js

--- app/forgot-mastercode/layout.js
+++ app/forgot-mastercode/layout.js
@@ -1,17 +1,15 @@
 
 
-import { ClerkProvider,SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs';
-import Manager from '../components/manager'; // Replace with your actual component
 
 
 export default function PasswordLayout({ children }) {
   return (
     <>
-    <ClerkProvider>
+    
 
       
       {children}
-    </ClerkProvider>
+    
     </>
   );
 }
