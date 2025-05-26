fe627d8680be922aea0c3c8e23652c594a363b4e
Abd-baap <abhigyaworkspace@gmail.com>
2025-05-26 03:12:25 -0700

    Update layout.js

--- app/password/layout.js
+++ app/password/layout.js
@@ -1,7 +1,7 @@
 
 
 import { ClerkProvider,SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs';
-import Manager from '../components/Manager'; // Replace with your actual component
+import Manager from '../components/manager'; // Replace with your actual component
 
 export const metadata = {
   title: "BitAegiris - Password Manager",
