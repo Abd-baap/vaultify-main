9ae8f53993edf9a922f159d2ca671c756fbfa24a
Abd-baap <abhigyaworkspace@gmail.com>
2025-05-26 22:02:15 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,26 +1,22 @@
-
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
-  '/', 
-  '/about', 
-  '/support', 
-  '/sitemap.xml', 
-  '/robots.txt'
+  '/',
+  '/about',
+  '/support',
+  '/sitemap.xml',
+  '/robots.txt',
 ])
 
-export default clerkMiddleware(async (auth, req) => {
-  // Only protect if it's NOT a public route
-  if (!isPublicRoute(req)) {
-    await auth().protect()
-  }
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth().protect()
+  }
 })
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
 }
