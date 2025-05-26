c0e59d9cfc36fe8b732adc889506206b56124e1f
Abd-baap <abhigyaworkspace@gmail.com>
2025-05-26 22:44:04 +0530

    Update layout.js

--- app/sign-in/layout.js
+++ app/sign-in/layout.js
@@ -1,15 +1,13 @@
 
 
-import { ClerkProvider,SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs';
-
 
 export default function PasswordLayout({ children }) {
   return (
     <>
-        <ClerkProvider>
+        
       
       {children}
-    </ClerkProvider>
+    
     </>
   );
 }
