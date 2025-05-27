9d74c3fd657eb72e50773cbaa94158df52ab0fc4
Abd-baap <abhigyaworkspace@gmail.com>
2025-05-27 02:09:03 -0700

    Update page.js

--- app/sign-in/page.js
+++ app/sign-in/page.js
@@ -1,10 +1,11 @@
 "use client"
 import { SignIn, useUser } from '@clerk/nextjs'
-
+import { useRouter } from 'next/navigation'; // For App Router
+// import { useRouter } from 'next/router'; // For Pages Router (older)
 export default function Home() {
   const { user } = useUser()
 
   if (!user) return <SignIn />
-
-  return <div>Welcome!</div>
+router.push('/password')
+  return <div>Redirecting to manager please wait!</div>
 }
