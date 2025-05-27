11aa1fc9d471dd7668b1a068a1ee8beeeb15665c
Abd-baap <abhigyaworkspace@gmail.com>
2025-05-27 01:44:02 -0700

    Update route.js

--- app/api/get/route.js
+++ app/api/get/route.js
@@ -3,7 +3,7 @@ import clientPromise from "@/lib/mongodb";
 
 export async function GET() {
   
-    const client = await clientPromise;
+    const client = await clientPromise()
     const db = client.db("Vaultify");
     const collection = db.collection("Credentials");
     
